7a37ce4cfc407889a91d1d8617a1f937ec25b8c7
MUSESTAR-Cloud <xsrhub@163.com>
2022-04-30 15:29:06 +0800

    Create moviehead.js

---
+++ files/moviehead.js
@@ -0,0 +1,15 @@
+document.write("<div class=\"menu\">");
+document.write("		<ul class=\"dmx l\">");
+document.write("			<li>");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/\">首页<\/a><\/li>");
+document.write("			<li>");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/muse.html\">缪斯星专区<\/a><\/li>");
+document.write("			<li>");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/animation.html\">动漫专区<\/a><\/li>");
+document.write("			<li class=\"nav\">");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/movie.html\">影视专区<\/a><\/li>");
+document.write("			<li>");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/app.html\">软件专区<\/a><\/li>");
+document.write("			<li>");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/r18.html\">R18专区<\/a><\/li>");
+document.write("	<\/div>");
