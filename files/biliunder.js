de62a441e8e206cc23a8b79a8e7584fd735fd020
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-01 19:15:19 +0800

    Create biliunder.js

---
+++ files/biliunder.js
@@ -0,0 +1,8 @@
+document.write("<br>");
+document.write("    <div class=\"foot\">");
+document.write("        <div class=\"footbg\">");
+document.write("<a>© 2022 Bilibili<\/a>");
+document.write("              <br>");
+document.write("           <a>Copyright © 2014-2022 <a href=\" https:\/\/musestar-cloud.gitHub.io\">MUSESTAR<\/a> All Rights Reserved<\/a>");
+document.write("		<br>");
+document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a>");
