e71e42d266ca46d062b50537cf07d3e1e1901cb4
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-02 20:15:04 +0800

    Update hd.js

--- files/hd.js
+++ files/hd.js
@@ -1,1 +1,2 @@
-document.write("<video src=\".\/files\/hd.mp4\" width='auto' height=\"165\" controls autoplay><\/video>");
+document.write("<video src=\"https:\/\/musestar-cloud.github.io\/files\/hd.mp4\" width='auto' height=\"165\" controls autoplay><\/video>");
+
