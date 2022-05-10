83292d7a49a77f16a042326be3cd347a223948c0
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-10 23:45:13 +0800

    Update biliunder.js

--- files/biliunder.js
+++ files/biliunder.js
@@ -7,6 +7,6 @@ document.write("           <a>Copyright © 2014-2022 <a href=\" https:\/\/musest
 document.write("		<br>");
 document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a>");
 document.write("		<br>");
-document.write("<a href=\"https:\/\/afdian.net\/@musestarxota" target=\"_blank\">爱发电<\/a>");
+document.write("<a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a>");
 document.write("		<br>");
 document.write("           <a>缪斯星官方QQ群：750813427<\/a>");
