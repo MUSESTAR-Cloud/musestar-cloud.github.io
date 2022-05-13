07363cdb2855744ed2959314bb27678dd8f228db
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-13 21:42:38 +0800

    Create under.js

---
+++ files/under.js
@@ -0,0 +1,5 @@
+document.write("           <p>Copyright © 2014-2022 <a href=\" https:\/\/musestar-cloud.gitHub.io\">MUSESTAR<\/a> All Rights Reserved<\/p>");
+document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a><br>");
+document.write("<a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a>");
+document.write("		<br>");
+document.write("           <a>缪斯星官方QQ群：750813427<\/a>");
