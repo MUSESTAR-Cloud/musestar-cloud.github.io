2236381142267d525194125a0516e81dfb817580
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-15 19:28:49 +0800

    Create afd.js

---
+++ files/afd.js
@@ -0,0 +1,13 @@
+document.write("    <div class=\"side r\">");
+document.write("    <div class=\"firs l\">");
+document.write("    <div class=\"dtit\">");
+document.write("        <h2>感谢<u><a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a><\/u>支持<\/h2>");
+document.write("    <\/div>");
+document.write("    <\/div>");
+document.write("	<div class=\"tlist\">");
+document.write("<li>");
+document.write("                <span>");
+document.write("                    <a>5-15<\/a><\/span>");
+document.write("                <a>缪斯星 - 10元<\/a>");
+document.write("            <\/li>");
+document.write("<\/div>      ");
