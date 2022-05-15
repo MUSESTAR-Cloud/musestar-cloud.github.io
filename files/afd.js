0b1ff9a1b0fa6791ed156c57e77026022d98f963
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-15 20:08:53 +0800

    Update afd.js

--- files/afd.js
+++ files/afd.js
@@ -1,13 +1,13 @@
 document.write("    <div class=\"side r\">");
 document.write("    <div class=\"firs l\">");
 document.write("    <div class=\"dtit\">");
-document.write("        <h2>感谢<u><a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a><\/u>支持<\/h2>");
+document.write("        <h2><u><a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a><\/u>支持榜<\/h2>");
 document.write("    <\/div>");
 document.write("    <\/div>");
 document.write("	<div class=\"tlist\">");
 document.write("<li>");
 document.write("                <span>");
 document.write("                    <a>缪斯星<\/a><\/span>");
-document.write("                <a>暂无支持，点击上方<u>爱发电<\/u>支持我们<\/a>");
+document.write("                <a>暂无人支持，点击上方<u>爱发电<\/u>支持我们<\/a>");
 document.write("            <\/li>");
 document.write("<\/div>      ");
