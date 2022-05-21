441cb2ecbb4bfef4cba7d178aaeeb724a81ac6a3
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-21 22:00:39 +0800

    Create search.js

---
+++ files/search.js
@@ -0,0 +1,26 @@
+<style type="text/css">
+    #box{
+        width: 380px;
+        margin: 30px auto;
+        font-family: 'Microsoft YaHei';
+        font-size: 14px;
+    }
+    input{
+        width: 260px;
+        border: 1px solid #e2e2e2;
+        height: 30px;
+        float: left;
+        background-image: url(images/search.jpg);
+        background-repeat: no-repeat;
+        background-size: 25px;
+        background-position:5px center;
+        padding:0 0 0 40px;
+    }
+</style>
+    <div id="box">
+<form class="navbar-form navbar-right site-search" action="http://cn.bing.com/search" target="_blank">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="搜索" name="q" value="                                                                                                    site:musestar-cloud.github.io">
+  </div>
+<button type="submit" class="btn btn-default">搜索</button></form>
+    </div>
