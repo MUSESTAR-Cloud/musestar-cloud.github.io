598b9a1f4c79df69994d92fd6388c7a1c75523cb
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-22 23:22:12 +0800

    1

---
+++ files/notice.js
@@ -0,0 +1,21 @@
+function narn (type) {
+   naranja()[type]({
+   title: '新消息提示',
+   text: '单击“接受”以创建新通知',
+   timeout: 'keep',
+   buttons: [{
+   text: '接受',
+   click: function (e) {
+   naranja().success({
+   title: '通知',
+   text: '通知被接受'
+  })
+ }
+},{
+   text: '取消',
+   click: function (e) {
+    e.closeNotification()
+   }
+  }]
+  })
+ }
