f0b7c27968485bb947f22d0ab1bb022c00195ffb
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-22 17:07:40 +0800

    3

--- r18/files/head.js
+++ r18/files/head.js
@@ -1,6 +1,8 @@
 document.write("	<div class=\"menu\">");
 document.write("		<ul class=\"dmx l\">");
 document.write("			<li>");
+document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/\"><img src=\"https:\/\/musestar-cloud.github.io\/ms.ico\" width=\"25\" height=\"25\"><\/a><\/li>");
+document.write("			<li>");
 document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/\">首页<\/a><\/li>");
 document.write("			<li>");
 document.write("				<a href=\"https:\/\/musestar-cloud.github.io\/muse.html\">缪斯星专区<\/a><\/li>");
