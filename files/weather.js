d74157e545a0d492c28f1d01a46999a3fb1a74e1
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-29 16:34:41 +0800

    Create weather.js

---
+++ files/weather.js
@@ -0,0 +1,1 @@
+document.write("<iframe scrolling=\"no\" src=\"https:\/\/tianqiapi.com\/api.php?style=tt&skin=pitaya\" frameborder=\"0\" width=\"590\" height=\"98\" allowtransparency=\"true\"><\/iframe>");
