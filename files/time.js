5e981497b6e490e9eb1407fc97ca919ee68f3a08
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-29 16:53:05 +0800

    XOTA

--- files/time.js
+++ files/time.js
@@ -1,4 +1,4 @@
-document.write("    <font size=\"10\"><div id=\"datetime\">");
+document.write("    <font size=\"5\"><div id=\"datetime\">");
 document.write("        <script>");
 document.write("            setInterval(\"document.getElementById('datetime').innerHTML=new Date().toLocaleString();\", 1000);");
 document.write("        <\/script>");
