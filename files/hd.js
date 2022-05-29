9bc8af6d4306c75da28b58a29f9a5a2587c79ca2
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-29 23:17:12 +0800

    活动

--- files/hd.js
+++ files/hd.js
@@ -1,1 +1,6 @@
+document.write("    <div class=\"side r\">");
+document.write("    <div class=\"firs l\">");
+document.write("    <div class=\"dtit\">");
+document.write("        <h2缪斯星活动<\/h2>");
 document.write("<img src=\"https:\/\/musestar-cloud.github.io\/files\/hd.jpg\" width='250' height=\"auto\" \/>");
+document.write("<\/div>      ");
