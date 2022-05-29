9d0818626f5e27a55164ae71d4f659d376bed001
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-29 23:21:47 +0800

    活动

--- files/hd.js
+++ files/hd.js
@@ -4,5 +4,5 @@ document.write("    <div class=\"dtit\">");
 document.write("        <h2缪斯星活动<\/h2>");
 document.write("    <\/div>");
 document.write("    <\/div>");
-document.write("<img src=\"https:\/\/musestar-cloud.github.io\/files\/hd.jpg\" width='250' height=\"auto\" \/>");
+document.write("<a href=\"http:\/\/www.news.cn\/politics\/2022-05\/29\/c_1128693876.htm\"><img src=\"https:\/\/musestar-cloud.github.io\/files\/hd.jpg\" width='250' height=\"auto\" \/><\/a>");
 document.write("<\/div>      ");
