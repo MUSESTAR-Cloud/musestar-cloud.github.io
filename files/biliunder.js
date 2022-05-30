a93359c3387853d4e1cdf5a79cbbb4a336711eea
MUSESTAR-Cloud <xsrhub@163.com>
2022-05-30 21:45:57 +0800

    QQ群

--- files/biliunder.js
+++ files/biliunder.js
@@ -9,6 +9,6 @@ document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_b
 document.write("		<br>");
 document.write("<a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a>");
 document.write("		<br>");
-document.write("           <a>缪斯星官方QQ群：750813427<\/a>");
+document.write("           <a href=\"https:\/\/jq.qq.com\/?_wv=1027&k=4A08XieB\" target=\"_blank\">缪斯星官方QQ群：750813427<\/a>");
 document.write("		<br>");
 document.write("<img alt=\"GitHub last commit\" src=\"https:\/\/img.shields.io\/github\/last-commit\/MUSESTAR-Cloud\/musestar-cloud.github.io\">");
--- files/under.js
+++ files/under.js
@@ -2,6 +2,6 @@ document.write("           <p>Copyright © 2014-2022 <a href=\" https:\/\/musest
 document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a><br>");
 document.write("<a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a>");
 document.write("		<br>");
-document.write("           <a>缪斯星官方QQ群：750813427<\/a>");
+document.write("           <a href=\"https:\/\/jq.qq.com\/?_wv=1027&k=4A08XieB\" target=\"_blank\">缪斯星官方QQ群：750813427<\/a>");
 document.write("		<br>");
 document.write("<img alt=\"GitHub last commit\" src=\"https:\/\/img.shields.io\/github\/last-commit\/MUSESTAR-Cloud\/musestar-cloud.github.io\">");
