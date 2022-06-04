e2f3c1ec096880b3b58c942bb18630ca0169aa1f
MUSESTAR-Cloud <xsrhub@163.com>
2022-06-05 01:49:30 +0800

    Update biliunder.js

--- files/biliunder.js
+++ files/biliunder.js
@@ -3,7 +3,7 @@ document.write("    <div class=\"foot\">");
 document.write("        <div class=\"footbg\">");
 document.write("<a>© 2022 Bilibili<\/a>");
 document.write("              <br>");
-document.write("           <a>Copyright © 2014-2022 <a href=\" https:\/\/musestar-cloud.gitHub.io\">MUSESTAR<\/a> All Rights Reserved<\/a>");
+document.write("           <a href=\" https:\/\/musestar-cloud.gitHub.io\">Copyright © 2014-2022 MUSESTAR All Rights Reserved<\/a>");
 document.write("		<br>");
 document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a>");
 document.write("		<br>");
