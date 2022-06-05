90efa369ab22eaa06e7391cc9f6024237555ce53
MUSESTAR-Cloud <xsrhub@163.com>
2022-06-05 21:49:02 +0800

    更新

--- files/biliunder.js
+++ files/biliunder.js
@@ -7,7 +7,7 @@ document.write("           <a href=\" https:\/\/musestar-cloud.gitHub.io\">Copyr
 document.write("		<br>");
 document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a>");
 document.write("		<br>");
-document.write("<a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a>");
+document.write("<a href=\"https:\/\/musestar-cloud.github.io\/pay.html\" target=\"_blank\">发电赞助<\/a>");
 document.write("		<br>");
 document.write("           <a href=\"https:\/\/jq.qq.com\/?_wv=1027&k=4A08XieB\" target=\"_blank\">缪斯星官方QQ群：750813427<\/a>");
 document.write("		<br>");
--- files/videounder.js
+++ files/videounder.js
@@ -11,7 +11,7 @@ document.write("    <div class=\"foot\">");
 document.write("        <div class=\"footbg\">");
 document.write("           <p><a href=\" https:\/\/musestar-cloud.gitHub.io\">Copyright © 2014-2022 MUSESTAR All Rights Reserved<\/a><\/p>");
 document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20220574\" target=\"_blank\">萌ICP备20220574号<\/a><br>");
-document.write("<a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">爱发电<\/a>");
+document.write("<a href=\"https:\/\/musestar-cloud.github.io\/pay.html\" target=\"_blank\">发电赞助<\/a>");
 document.write("		<br>");
 document.write("           <a href=\"https:\/\/jq.qq.com\/?_wv=1027&k=4A08XieB\" target=\"_blank\">缪斯星官方QQ群：750813427<\/a>");
 document.write("		<br>");
