bf6dbfe1c7d973be2d98c5212569d30e6218ef2e
MUSESTAR-Cloud <xsrhub@163.com>
2022-06-05 21:04:26 +0800

    Update afd.js

--- files/afd.js
+++ files/afd.js
@@ -1,14 +1,14 @@
 document.write("    <div class=\"side r\">");
 document.write("    <div class=\"firs l\">");
 document.write("    <div class=\"dtit\">");
-document.write("        <h2><u><a href=\"https:\/\/afdian.net\/@musestarxota\" target=\"_blank\">发电<\/a><\/u>支持榜<\/h2>");
+document.write("        <h2><u><a href=\"https:\/\/musestar-cloud.github.io\/pay.html\" target=\"_blank\">发电<\/a><\/u>支持榜<\/h2>");
 document.write("    <\/div>");
 document.write("    <\/div>");
 document.write("	<div class=\"tlist\">");
 document.write("<li>");
 document.write("                <span>");
-document.write("                    <a>5-20<\/a><\/span>");
-document.write("                <a>留白支持了8.5元<\/a>");
+document.write("                    <a>6-08<\/a><\/span>");
+document.write("                <a>麻油支持了8元<\/a>");
 document.write("            <\/li>");
 document.write("<li>");
 document.write("                <span>");
