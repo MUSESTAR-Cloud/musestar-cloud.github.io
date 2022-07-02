2616ce02e7b5f94115a6bbfbbebe70476da7c593
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-02 20:58:57 +0800

    更新

--- files/under.js
+++ files/under.js
@@ -5,7 +5,7 @@ document.write("           <p><a href=\" https:\/\/musestar-cloud.gitHub.io\">Co
 document.write("<a href=\"https:\/\/musestar-cloud.github.io\/icp.html\" target=\"_blank\">萌ICP备20220574号<\/a><br>");
 document.write("<a href=\"https:\/\/musestar-cloud.github.io\/FAQ.html\" target=\"_blank\">投稿反馈<\/a>");
 document.write("		<br>");
-document.write("<a href=\"https:\/\/musestar-cloud.github.io\/payhtml\" target=\"_blank\">发电赞助<\/a>");
+document.write("<a href=\"https:\/\/musestar-cloud.github.io\/pay.html\" target=\"_blank\">发电赞助<\/a>");
 document.write("		<br>");
 document.write("           <a href=\"https:\/\/jq.qq.com\/?_wv=1027&k=4A08XieB\" target=\"_blank\">缪斯星官方QQ群：750813427<\/a>");
 document.write("		<br>");
