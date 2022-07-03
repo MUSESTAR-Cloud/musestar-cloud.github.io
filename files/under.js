18ca47792b403204611ee8ec06a5c14594710081
MUSESTAR-Cloud <xsrhub@163.com>
2022-07-03 22:38:36 +0800

    更新

--- files/under.js
+++ files/under.js
@@ -1,14 +1,8 @@
 document.write("<br>");
 document.write("    <div class=\"foot\">");
 document.write("        <div class=\"footbg\">");
-document.write("           <p><a href=\" https:\/\/musestar-cloud.gitHub.io\">Copyright © 2014-2022 MUSESTAR All Rights Reserved<\/a><\/p>");
+document.write("           <a href=\" https:\/\/musestar-cloud.gitHub.io\">Copyright © 2014-2022 MUSESTAR All Rights Reserved<\/a>");
 document.write("<a href=\"https:\/\/musestar-cloud.github.io\/icp.html\" target=\"_blank\">萌ICP备20220574号<\/a><br>");
-document.write("<a href=\"https:\/\/musestar-cloud.github.io\/FAQ.html\" target=\"_blank\">投稿反馈<\/a>");
-document.write("		<br>");
-document.write("<a href=\"https:\/\/musestar-cloud.github.io\/pay.html\" target=\"_blank\">发电赞助<\/a>");
-document.write("		<br>");
-document.write("           <a href=\"https:\/\/jq.qq.com\/?_wv=1027&k=4A08XieB\" target=\"_blank\">缪斯星官方QQ群：750813427<\/a>");
-document.write("		<br>");
 document.write("           <a target=\"_blank\">本站只提供WEB页面服务，本站不存储、不制作任何视频，不承担任何由于内容的合法性及健康性所引起的争议和法律责任。<\/a>");
 document.write("		<br>");
 document.write("<img alt=\"GitHub last commit\" src=\"https:\/\/img.shields.io\/github\/last-commit\/MUSESTAR-Cloud\/musestar-cloud.github.io\">");
